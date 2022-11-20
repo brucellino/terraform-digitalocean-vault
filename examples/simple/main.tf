2abcacd7671f97f44733e0ab67d28a762523633a
Bruce Becker <brucellino@protonmail.com>
2022-11-20 09:40:05 +0100

    Fix/vpc test (#1)
    chore: add terraform lock for example
    fix(provider,vault): use less constraint in terraform required provider
    chore: add lock for module
    fix(droplet): fix image lookup for base droplet
    fix(templates): use go template syntax for interface listener config
    test: add vpc module before droplets
    ci: add consul to release action
    ci: start consul in the background
    
    Signed-off-by: Bruce Becker <brucellino@protonmail.com>

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -1,21 +1,55 @@
-# This is the default example
-# customise it as you see fit for your example usage of your module
+terraform {
+  backend "consul" {
+    path = "terraform/modules/digitalocean-vault/test"
+  }
+  required_providers {
+    vault = {
+      source  = "hashicorp/vault"
+      version = "3.11.0"
+    }
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "2.24.0"
+    }
+  }
+}
 
-# add provider configurations here, for example:
-# provider "aws" {
-#
-# }
+variable "do_kv_mount_path" {
+  type    = string
+  default = "digitalocean"
+}
 
-# Declare your backends and other terraform configuration here
-# This is an example for using the consul backend.
-# terraform {
-#   backend "consul" {
-#     path = "test_module/simple"
-#   }
-# }
+variable "project" {
+  type = map(string)
+  default = {
+    description = "Vault Project"
+    environment = "development"
+    name        = "Vault module test"
+    purpose     = "Personal"
+  }
+}
+data "vault_kv_secret_v2" "do" {
+  mount = var.do_kv_mount_path
+  name  = "tokens"
+}
 
+provider "vault" {}
+provider "digitalocean" {
+  token = data.vault_kv_secret_v2.do.data["terraform"]
+}
 
-module "example" {
-  source = "../../"
-  dummy  = "test"
+module "vpc" {
+  source     = "brucellino/vpc/digitalocean"
+  version    = "1.0.3"
+  project    = var.project
+  vpc_name   = "vault-test"
+  vpc_region = "ams3"
+}
+
+module "cluster" {
+  depends_on               = [module.vpc]
+  source                   = "../../"
+  vpc_name                 = "vault-test"
+  project_name             = var.project.name
+  ssh_inbound_source_cidrs = ["2.44.137.42"]
 }
